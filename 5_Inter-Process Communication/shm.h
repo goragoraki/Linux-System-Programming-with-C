b4a8b8a456a98a05c2a64aa7c5948344fda14e5b
Ko-SungHoon <89345673+goragoraki@users.noreply.github.com>
2023-01-12 00:12:19 +0900

    Delete 5_Inter-Process Communication directory

--- 5_Inter-Process Communication/shm.h
+++
@@ -1,4 +0,0 @@
-#define MY_ID 2
-#define SHM_KEY (0x9000 + MY_ID) // shared memory 고유 key 
-#define SHM_SIZE 1024 // memory size
-#define SHM_MODE (SHM_R|SHM_W|IPC_CREAT)
--- 5_Inter-Process Communication/synclib.h
+++
@@ -1,5 +0,0 @@
-void TELL_WAIT(void);
-void TELL_PARENT(void);
-void TELL_CHILD(void);
-void WAIT_CHILD(void);
-void WAIT_PARENT(void);
